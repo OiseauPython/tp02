4dab4806fb26c25592bed7b7e538feef643bafd7
jérémy stanko <stankojeremy96@gmail.com>
2020-04-17 14:26:54 +0200

    add the geolocation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {AlertController} from '@ionic/angular';
+import {AlertController, LoadingController} from '@ionic/angular';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +10,10 @@ import {AlertController} from '@ionic/angular';
 export class HomePage {
 
   title: string;
+  lat:any=''
+  lng:any=''
 
-  constructor(private alertController: AlertController) {}
+  constructor(private alertController: AlertController, public loadingController: LoadingController, private geolocation: Geolocation) {}
 
   updateTitle() {
     this.title = 'Mon Nouveau Titre';
@@ -31,6 +34,42 @@ export class HomePage {
     alert.onDidDismiss().then(() => console.log('alerte masquée'))
 
     // affichage de l alerte
+    await alert.present();
+  }
+
+  async getLoc(){
+
+    const loading = await this.loadingController.create({
+      message: 'Please wait...',
+      });
+    await loading.present();
+    this.geolocation.getCurrentPosition(
+      {maximumAge: 1000, timeout: 5000,
+       enableHighAccuracy: true }
+      ).then((resp) => {
+            // resp.coords.latitude
+            // resp.coords.longitude
+            //alert("r succ"+resp.coords.latitude)
+            alert(JSON.stringify( resp.coords));
+            loading.dismiss()
+            this.lat=resp.coords.latitude
+            this.lng=resp.coords.longitude
+            },er=>{
+              //alert("error getting location")
+              alert('Can not retrieve Location')
+            }).catch((error) => {
+            //alert('Error getting location'+JSON.stringify(error));
+            alert('Error getting location - '+JSON.stringify(error))
+            });
+  }
+  
+  async showLoader(msg)
+  {
+    const alert = await this.alertController.create({
+      message: msg,
+      buttons: ['OK']
+    });
+
     await alert.present();
   }
 
